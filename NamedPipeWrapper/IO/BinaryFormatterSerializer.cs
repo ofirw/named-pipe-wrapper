031315ad6ce248b5816cc9d5fd1dc50da8da76af
Ofir Winegarten <ofir.win@gmail.com>
2017-05-09 17:21:48 +0300

    Added a protobuf serializer in a new project

---
+++ NamedPipeWrapper/IO/BinaryFormatterSerializer.cs
@@ -0,0 +1,41 @@
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace NamedPipeWrapper.IO
+{
+    /// <summary>
+    /// Uses BinaryFormatter of serialization
+    /// </summary>
+    public class BinaryFormatterSerializer<T> : ISerializer<T>
+    {
+        /// <exception cref="SerializationException">An object in the graph of type parameter <typeparamref name="T"/> is not marked as serializable.</exception>
+        public byte[] Serialize(T obj)
+        {
+            try
+            {
+                
+                using (var memoryStream = new MemoryStream())
+                {
+                    var binaryFormatter = new BinaryFormatter();
+                    binaryFormatter.Serialize(memoryStream, obj);
+                    return memoryStream.ToArray();
+                }
+            }
+            catch
+            {
+                //if any exception in the serialize, it will stop named pipe wrapper, so there will ignore any exception.
+                return null;
+            }
+        }
+
+        public T Deserialize(byte[] data)
+        {
+            using (var memoryStream = new MemoryStream(data))
+            {
+                var binaryFormatter = new BinaryFormatter();
+                return (T)binaryFormatter.Deserialize(memoryStream);
+            }
+        }
+    }
+}
---
+++ protobuf-net-serializer/ProtobufNetSerializer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProtoBuf;
+
+namespace NamedPipeWrapper.IO
+{
+    public class ProtobufNetSerializer<T> : ISerializer<T>
+    {
+        public byte[] Serialize(T obj)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                Serializer.Serialize(memoryStream, obj);
+                return memoryStream.ToArray();
+            }
+        }
+
+        public T Deserialize(byte[] data)
+        {
+            using (var memoryStream = new MemoryStream(data))
+            {
+                return Serializer.Deserialize<T>(memoryStream);
+            }
+        }
+    }
+}
